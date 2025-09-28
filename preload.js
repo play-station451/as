cafbba0a744e88d0c40213ffb363feb05d59ffde
playstation451 <7b7t.net@gmail.com>
2025-09-28 18:56:05 +0100

    a

--- preload.js
+++ preload.js
@@ -6,6 +6,19 @@ contextBridge.exposeInMainWorld(
   'electron',
   {
     ipcRenderer: {
+      send: (channel, data) => {
+        console.log(`Sending IPC message: ${channel}`);
+        ipcRenderer.send(channel, data);
+      },
+      on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
+      once: (channel, func) => ipcRenderer.once(channel, (event, ...args) => func(...args)),
+      closeWindow: () => ipcRenderer.send('close-window'),
+      toggleMaximizeWindow: () => ipcRenderer.send('toggle-maximize-window')
+    }
+  },
+  'electron',
+  {
+    ipcRenderer: {
       send: (channel, data) => ipcRenderer.send(channel, data),
       on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
       once: (channel, func) => ipcRenderer.once(channel, (event, ...args) => func(...args)),
